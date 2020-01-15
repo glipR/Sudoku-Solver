3c82b26682271f5d7c00a7a349b6170fe4d5607d
glipR <jgoe0001@student.monash.edu>
2020-01-15 20:56:47 +1100

    Add box spawning and fix margin errors.

--- Assets/VisualBoard/Scripts/VisualBoardController.cs
+++ Assets/VisualBoard/Scripts/VisualBoardController.cs
@@ -21,6 +21,8 @@ public class VisualBoardController : MonoBehaviour {
     private GameObject ThickLine = null;
     [SerializeField]
     private GameObject ThinLine = null;
+    [SerializeField]
+    private GameObject BoxObject = null;
     private Vector2 backdropDimensions;
 
     // Cached display properties
@@ -29,21 +31,27 @@ public class VisualBoardController : MonoBehaviour {
     private Vector2 largeLineLengths;
     private Vector2 smallLineLengths;
     private float thickWidth;
+    private float thinWidth;
     private GameObject backdrop;
+
+    // Publically accessible boxes.
+    public BoxController[,] boxes;
 
     private void Start() {
         GenerateDisplayConstants();
         GenerateBorders();
+        GenerateBoxes();
     }
 
     private void GenerateDisplayConstants() {
         backdrop = transform.Find("WhiteBackdrop").gameObject;
         backdropDimensions = backdrop.GetComponent<RectTransform>().sizeDelta;
         thickWidth = ThickLine.GetComponent<RectTransform>().sizeDelta.y;
+        thinWidth = ThinLine.GetComponent<RectTransform>().sizeDelta.y;
         beginningVerticalPos = new Vector2(0, -thickWidth / 2f);
         beginningHorizontalPos = new Vector2(-backdropDimensions.x / 2f + thickWidth / 2f, -backdropDimensions.y / 2f);
-        largeLineLengths = new Vector2((backdropDimensions.y - thickWidth) / (settings.numVerticalThicks - 1f), (backdropDimensions.x - thickWidth) / (settings.numHorizontalThicks - 1f));
-        smallLineLengths = new Vector2(largeLineLengths.x / (settings.numVerticalThins + 1f), largeLineLengths.y / (settings.numHorizontalThins + 1f));
+        largeLineLengths = new Vector2((backdropDimensions.x - thickWidth) / (settings.numHorizontalThicks - 1f), (backdropDimensions.y - thickWidth) / (settings.numVerticalThicks - 1f));
+        smallLineLengths = new Vector2((largeLineLengths.x - thickWidth + thinWidth) / (settings.numHorizontalThins + 1f), (largeLineLengths.y - thickWidth + thinWidth) / (settings.numVerticalThins + 1f));
     }
 
     private void GenerateBorders() {
@@ -52,27 +60,49 @@ public class VisualBoardController : MonoBehaviour {
         for (int thickIndex=0; thickIndex<settings.numVerticalThicks; thickIndex++) {
             RectTransform top_line = Instantiate(ThickLine, backdrop.transform).GetComponent<RectTransform>();
             top_line.gameObject.name = "VerticalThick " + thickIndex;
-            top_line.anchoredPosition = beginningVerticalPos + new Vector2(0, -largeLineLengths.x * thickIndex);
+            top_line.anchoredPosition = beginningVerticalPos + new Vector2(0, -largeLineLengths.y * thickIndex);
             if (thickIndex != settings.numVerticalThicks-1)
             for (int thinIndex=1; thinIndex<=settings.numVerticalThins; thinIndex++) {
                 RectTransform thin_line = Instantiate(ThinLine, backdrop.transform).GetComponent<RectTransform>();
                 thin_line.gameObject.name = "VerticalThin " + thickIndex + " " + thinIndex;
-                thin_line.anchoredPosition = top_line.anchoredPosition - new Vector2(0, smallLineLengths.x * thinIndex);
+                thin_line.anchoredPosition = top_line.anchoredPosition - new Vector2(0, (thickWidth - thinWidth) / 2f + smallLineLengths.y * thinIndex);
             }
         }
         for (int thickIndex=0; thickIndex<settings.numHorizontalThicks; thickIndex++) {
             RectTransform left_line = Instantiate(ThickLine, backdrop.transform).GetComponent<RectTransform>();
             left_line.gameObject.name = "HorizontalThick " + thickIndex;
-            left_line.anchoredPosition = beginningHorizontalPos + new Vector2(largeLineLengths.y * thickIndex , 0f);
+            left_line.anchoredPosition = beginningHorizontalPos + new Vector2(largeLineLengths.x * thickIndex , 0f);
             left_line.rotation = Quaternion.AngleAxis(90f, Vector3.forward);
+            if (thickIndex != settings.numHorizontalThicks-1)
             for (int thinIndex=1; thinIndex<=settings.numHorizontalThins; thinIndex++) {
                 RectTransform thin_line = Instantiate(ThinLine, backdrop.transform).GetComponent<RectTransform>();
                 thin_line.gameObject.name = "HorizontalThin " + thickIndex + " " + thinIndex;
                 thin_line.rotation = Quaternion.AngleAxis(90f, Vector3.forward);
-                thin_line.anchoredPosition = left_line.anchoredPosition + new Vector2(smallLineLengths.y * thinIndex, 0);
+                thin_line.anchoredPosition = left_line.anchoredPosition + new Vector2((thickWidth - thinWidth) / 2f + smallLineLengths.x * thinIndex, 0);
             }
         }
 
+    }
+
+    private void GenerateBoxes() {
+        boxes = new BoxController[(settings.numHorizontalThicks - 1) * (settings.numHorizontalThins - 1), (settings.numVerticalThicks - 1) * (settings.numVerticalThins + 1)];
+        for (int thickH=0; thickH < settings.numHorizontalThicks-1; thickH++) {
+            for (int thickV=0; thickV < settings.numVerticalThicks-1; thickV++) {
+                for (int thinH=0; thinH < settings.numHorizontalThins+1; thinH++) {
+                    for (int thinV=0; thinV < settings.numVerticalThins+1; thinV++) {
+                        RectTransform box = Instantiate(BoxObject, backdrop.transform).GetComponent<RectTransform>();
+                        box.gameObject.name = "Box (" + (thickH * (settings.numHorizontalThins + 1) + thinH) + ", " + (thickV * (settings.numVerticalThins + 1) + thinV) + ")";
+                        box.anchoredPosition = new Vector2(
+                            thickWidth + (smallLineLengths.x - thinWidth) / 2f + largeLineLengths.x * thickH + smallLineLengths.x * thinH,
+                            -thickWidth - (smallLineLengths.y - thinWidth) / 2f - largeLineLengths.y * thickV - smallLineLengths.y * thinV
+                        );
+                        box.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, smallLineLengths.y - thinWidth);
+                        box.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, smallLineLengths.x - thinWidth);
+                        boxes[thickH * (settings.numHorizontalThins + 1) + thinH, thickV * (settings.numVerticalThins + 1) + thinV] = box.GetComponent<BoxController>();
+                    }
+                }
+            }
+        }
     }
 
 }
