5a8bb1e82273cf104c901970f3b805d27e00bdb8
glipR <jgoe0001@student.monash.edu>
2020-01-15 17:41:15 +1100

    Refactor Line placer to expose display constants.

--- Assets/VisualBoard/Scripts/VisualBoardController.cs
+++ Assets/VisualBoard/Scripts/VisualBoardController.cs
@@ -23,36 +23,53 @@ public class VisualBoardController : MonoBehaviour {
     private GameObject ThinLine = null;
     private Vector2 backdropDimensions;
 
+    // Cached display properties
+    private Vector2 beginningVerticalPos;
+    private Vector2 beginningHorizontalPos;
+    private Vector2 largeLineLengths;
+    private Vector2 smallLineLengths;
+    private float thickWidth;
+    private GameObject backdrop;
+
     private void Start() {
-        backdropDimensions = transform.Find("WhiteBackdrop").GetComponent<RectTransform>().sizeDelta;
+        GenerateDisplayConstants();
         GenerateBorders();
     }
 
+    private void GenerateDisplayConstants() {
+        backdrop = transform.Find("WhiteBackdrop").gameObject;
+        backdropDimensions = backdrop.GetComponent<RectTransform>().sizeDelta;
+        thickWidth = ThickLine.GetComponent<RectTransform>().sizeDelta.y;
+        beginningVerticalPos = new Vector2(0, -thickWidth / 2f);
+        beginningHorizontalPos = new Vector2(-backdropDimensions.x / 2f + thickWidth / 2f, -backdropDimensions.y / 2f);
+        largeLineLengths = new Vector2((backdropDimensions.y - thickWidth) / (settings.numVerticalThicks - 1f), (backdropDimensions.x - thickWidth) / (settings.numHorizontalThicks - 1f));
+        smallLineLengths = new Vector2(largeLineLengths.x / (settings.numVerticalThins + 1f), largeLineLengths.y / (settings.numHorizontalThins + 1f));
+    }
+
     private void GenerateBorders() {
-        var backdrop = transform.Find("WhiteBackdrop");
 
         // Create the thick lines, and then each thin line.
         for (int thickIndex=0; thickIndex<settings.numVerticalThicks; thickIndex++) {
             RectTransform top_line = Instantiate(ThickLine, backdrop.transform).GetComponent<RectTransform>();
             top_line.gameObject.name = "VerticalThick " + thickIndex;
-            top_line.anchoredPosition = new Vector2(0, -top_line.sizeDelta.y / 2f - (backdropDimensions.y - top_line.sizeDelta.y) * thickIndex / (settings.numVerticalThicks - 1f));
+            top_line.anchoredPosition = beginningVerticalPos + new Vector2(0, -largeLineLengths.x * thickIndex);
             if (thickIndex != settings.numVerticalThicks-1)
             for (int thinIndex=1; thinIndex<=settings.numVerticalThins; thinIndex++) {
                 RectTransform thin_line = Instantiate(ThinLine, backdrop.transform).GetComponent<RectTransform>();
                 thin_line.gameObject.name = "VerticalThin " + thickIndex + " " + thinIndex;
-                thin_line.anchoredPosition = top_line.anchoredPosition - new Vector2(0, (backdropDimensions.y - top_line.sizeDelta.y) / (settings.numVerticalThicks - 1f) * (thinIndex / (float)(settings.numVerticalThins+1)));
+                thin_line.anchoredPosition = top_line.anchoredPosition - new Vector2(0, smallLineLengths.x * thinIndex);
             }
         }
         for (int thickIndex=0; thickIndex<settings.numHorizontalThicks; thickIndex++) {
             RectTransform left_line = Instantiate(ThickLine, backdrop.transform).GetComponent<RectTransform>();
             left_line.gameObject.name = "HorizontalThick " + thickIndex;
-            left_line.anchoredPosition = new Vector2(-backdropDimensions.x / 2f + left_line.sizeDelta.y / 2f + (backdropDimensions.x - left_line.sizeDelta.y) * thickIndex / (settings.numHorizontalThicks - 1f), -backdropDimensions.y / 2f);
+            left_line.anchoredPosition = beginningHorizontalPos + new Vector2(largeLineLengths.y * thickIndex , 0f);
             left_line.rotation = Quaternion.AngleAxis(90f, Vector3.forward);
             for (int thinIndex=1; thinIndex<=settings.numHorizontalThins; thinIndex++) {
                 RectTransform thin_line = Instantiate(ThinLine, backdrop.transform).GetComponent<RectTransform>();
                 thin_line.gameObject.name = "HorizontalThin " + thickIndex + " " + thinIndex;
                 thin_line.rotation = Quaternion.AngleAxis(90f, Vector3.forward);
-                thin_line.anchoredPosition = left_line.anchoredPosition + new Vector2((backdropDimensions.x - left_line.sizeDelta.y) / (settings.numHorizontalThicks - 1f) * (thinIndex / ((float)settings.numVerticalThins+1)), 0);
+                thin_line.anchoredPosition = left_line.anchoredPosition + new Vector2(smallLineLengths.y * thinIndex, 0);
             }
         }
 
