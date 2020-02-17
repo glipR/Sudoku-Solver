79555d28e34f16cb1a0227aceda8c57c609b1b7b
glipR <jgoe0001@student.monash.edu>
2020-02-17 21:44:27 +1100

    Add functional tags dropdown.

---
+++ Assets/Menu/Scripts/CreateModal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CreateModal : MonoBehaviour {
+
+    public GameObject VariantSuggestionsPrefab;
+    public List<GameObject> VariantSuggestions;
+
+    private void Awake() {
+        var tagInp = transform.Find("Tags").GetComponent<TMP_InputField>();
+        tagInp.onValueChanged.AddListener((s) => {
+            SetTagsDropdown(VariantList.SuggestedVariants(s));
+        });
+        tagInp.onSubmit.AddListener((s) => {
+            var res = VariantList.SuggestedVariants(s);
+            if (res.Count == 0) return;
+            ToggleVariant(res[0]);
+            EventSystem.current.SetSelectedGameObject(transform.Find("Tags").gameObject, null);
+            transform.Find("Tags").GetComponent<TMP_InputField>().ActivateInputField();
+        });
+        VariantSuggestions = new List<GameObject>();
+        for (int i=0; i<5; i++) {
+            var g = Instantiate(VariantSuggestionsPrefab, transform);
+            g.transform.localPosition += new Vector3(0, -i * g.GetComponent<RectTransform>().sizeDelta.y, 0);
+            g.SetActive(false);
+            VariantSuggestions.Add(g);
+        }
+    }
+
+    public void SetTagsDropdown(List<VariantList.VariantType> v) {
+        foreach (GameObject g in VariantSuggestions) g.SetActive(false);
+        int i=0;
+        foreach (VariantList.VariantType x in v) {
+            VariantSuggestions[i].transform.Find("Text").GetComponent<TextMeshProUGUI>().text = x.ToString();
+            VariantSuggestions[i].transform.GetComponent<Button>().onClick.AddListener(() => ToggleVariant(x));
+            VariantSuggestions[i].SetActive(true);
+            i++;
+        }
+    }
+
+    public void ToggleVariant(VariantList.VariantType v) {
+        foreach (GameObject g in VariantSuggestions) g.SetActive(false);
+        var tagInp = this.transform.Find("Tags").GetComponent<TMP_InputField>();
+        tagInp.text = "";
+    }
+
+}
--- Assets/Variants/VariantList.cs
+++ Assets/Variants/VariantList.cs
@@ -16,6 +16,34 @@ public static class VariantList {
         Thermo
     }
 
+    public static List<VariantType> SuggestedVariants(string searchTerm) {
+        searchTerm = searchTerm.ToLower();
+        List<VariantType> values = new List<VariantType>();
+        List<(VariantType var, float s)> scores = new List<(VariantType var, float s)>();
+        foreach (VariantType v in System.Enum.GetValues(typeof(VariantType))) {
+            // Check if searchTerm is subsequence of
+            string check = v.ToString().ToLower();
+            int stringIndex = 0;
+            float totalScore = 0;
+            int i=0;
+            for (i=0; i<check.Length; i++) {
+                if (stringIndex == searchTerm.Length) break;
+                if (searchTerm[stringIndex] == check[i]) {
+                    stringIndex++;
+                    totalScore += i;
+                }
+            }
+            if (stringIndex < searchTerm.Length) continue;
+            // Ensure shorter strings are matched first.
+            totalScore += searchTerm.Length / (float)check.Length;
+            // Lower totalScore is good.
+            scores.Add((v, totalScore));
+        }
+        scores.Sort((a, b) => a.s.CompareTo(b.s));
+        foreach ((VariantType var, int s) r in scores) values.Add(r.var);
+        return values;
+    }
+
     public static Variant GetVariant(string v) {
         if (v == VariantType.Base.ToString())
             return BaseVariant();
