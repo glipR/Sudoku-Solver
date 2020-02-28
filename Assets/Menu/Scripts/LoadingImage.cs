34802130c53dc5e7ca9d5751b821206051d7b0a8
glipR <jgoe0001@student.monash.edu>
2020-02-28 11:28:29 +1100

    Add spinning loading symbol.

---
+++ Assets/Menu/Scripts/LoadingImage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRuby.Tween;
+
+public class LoadingImage : MonoBehaviour {
+
+    public float spinWait;
+    public float spinDuration;
+
+    private float time;
+
+    private void Start() {
+        time = 0;
+    }
+
+    private void Update() {
+        float newTime = time + Time.deltaTime;
+        if (Mathf.Floor(time / spinWait) < Mathf.Floor(newTime / spinWait)) {
+            gameObject.Tween("RotateLoad", 0f, 360f, spinDuration, TweenScaleFunctions.QuadraticEaseInOut, (t)=> {
+                transform.localRotation = Quaternion.Euler(0f, 0f, t.CurrentValue);
+            });
+        }
+        time = newTime;
+    }
+
+}
